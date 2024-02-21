0df7821c31497195c268deec8dc7d3d9d6202b20
mdyeasinhn <mdyeasinhn@gmail.com>
2024-02-21 08:39:40 +0600

    function added

--- JS/alpha-clash.js
+++ JS/alpha-clash.js
@@ -1,3 +1,21 @@
+// function play(){ 
+//     //step 1: hidden the home screen.to hide the screen add the class hidden to the home section
+//     const homeScreen = document.getElementById('home-screen');
+//     homeScreen.classList.add('hidden');
+//     // step 2: remove the section
+//     const playgroundSection = document.getElementById('playground');
+//     playgroundSection.classList.remove('hidden');
+// }
+
+
+function continueGame(){
+    const alphabet = getARandomAlphabet();
+    console.log('Your random alphabet',alphabet)
+
+}
 function play(){
-    console.log('play start now')
+    hideElementById('home-screen');
+    showElementById('playground');
+    continueGame()
+
 }
